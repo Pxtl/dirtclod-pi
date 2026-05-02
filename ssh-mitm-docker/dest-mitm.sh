471d83c94716d5f6fdbd8ee8d5198605faefe2d0
Martin Zarate (aka Pxtl) <pxtl@hotmail.com>
2026-05-02 11:37:18 -0400

    fix dest-mitm.sh to properly forward git commands

--- ssh-mitm-docker/dest-mitm.sh
+++ ssh-mitm-docker/dest-mitm.sh
@@ -4,5 +4,10 @@
 HOME=/home/git
 export HOME
 
-# Force SSH to use the git user's config file
-exec ssh -F /home/git/.ssh/config -T dest "$@"
+# Forward the original command from the incoming SSH session.
+# When sshd is using ForceCommand, the requested command is available in SSH_ORIGINAL_COMMAND.
+if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
+    exec ssh -F /home/git/.ssh/config -T dest "$SSH_ORIGINAL_COMMAND"
+else
+    exec ssh -F /home/git/.ssh/config -T dest
+fi
